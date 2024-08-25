859cf971d5da73adb776c59c18dd9ced2fabdd68
LazyMG <cbfmark@gmail.com>
2024-08-25 17:47:27 +0900

    first deploy search suspense #2

--- app/(home)/search/page.tsx
+++ app/(home)/search/page.tsx
@@ -7,7 +7,7 @@ import { searchTweet } from "./actions";
 import Tweet from "@/components/tweet";
 import { Tweet as TweetType } from "@/lib/types";
 
-const Search = () => {
+const SearchContent = () => {
   const searchParams = useSearchParams();
   const keyword = searchParams.get("keyword") || "";
 
@@ -25,30 +25,34 @@ const Search = () => {
   }, [keyword]);
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="flex flex-col gap-5 py-8 px-6">
-        <div className="flex flex-col gap-1 font-semibold text-lg">
-          <span>{keyword} 검색 결과</span>
-        </div>
-        <div className="h-0.5 bg-neutral-100" />
-        {isLoading ? (
-          <div>Loading...</div>
-        ) : (
-          <div className="flex flex-col gap-4">
-            {tweets?.map((tweet) => (
-              <Tweet
-                key={tweet.id}
-                tweetDate={tweet.created_at}
-                tweet={tweet.tweet}
-                tweetId={tweet.id}
-                tweetUser={tweet.user.username}
-              />
-            ))}
-          </div>
-        )}
+    <div className="flex flex-col gap-5 py-8 px-6">
+      <div className="flex flex-col gap-1 font-semibold text-lg">
+        <span>{keyword} 검색 결과</span>
       </div>
-    </Suspense>
+      <div className="h-0.5 bg-neutral-100" />
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="flex flex-col gap-4">
+          {tweets?.map((tweet) => (
+            <Tweet
+              key={tweet.id}
+              tweetDate={tweet.created_at}
+              tweet={tweet.tweet}
+              tweetId={tweet.id}
+              tweetUser={tweet.user.username}
+            />
+          ))}
+        </div>
+      )}
+    </div>
   );
 };
 
-export default Search;
+export function Searchbar() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchContent />
+    </Suspense>
+  );
+}
